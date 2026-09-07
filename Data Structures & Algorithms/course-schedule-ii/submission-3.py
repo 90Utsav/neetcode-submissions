116be07517e8d06b910e6035bee69cb2bf3c5b55
Utsav B K <148077734+90Utsav@users.noreply.github.com>
2026-09-07 10:00:37 -0700

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-3.py
@@ -0,0 +1,38 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        from collections import deque 
+
+        q=deque()
+        n=numCourses
+        adj_list=[]
+        indegree=[0]*n 
+        ans=[] 
+
+        #making adj_list: 
+        for i in range(n): 
+             adj_list.append([])
+
+
+        for target, prereq in prerequisites:
+            adj_list[prereq].append(target)
+        #inordering: 
+        for a,b in prerequisites: 
+            indegree[a]+=1
+        
+        for i in range(n): 
+            if indegree[i] == 0: 
+                q.appendleft(i)
+                ans.append(i)
+
+        while len(q) > 0: 
+            front = q.pop()
+
+            for i in adj_list[front]: 
+                indegree[i]-=1
+                if indegree[i]==0: 
+                    q.appendleft(i)
+                    ans.append(i)
+            
+        return ans if len(ans) == n else []
+
+            
